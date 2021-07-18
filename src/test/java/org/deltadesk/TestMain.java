43bdbc0fe9a79a12e19d9516ec527938a4ee30f0
Andreas Meyer <andreas.meyer@deltadesk.org>
2021-07-18 13:12:57 +0200

    Added maven pom and some sub files

---
+++ src/test/java/org/deltadesk/TestMain.java
@@ -0,0 +1,14 @@
+package org.deltadesk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
