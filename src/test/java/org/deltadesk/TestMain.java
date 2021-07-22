f860c7920b2c5cb0e7acb90c3e2da694d214f644
Andreas Meyer <andreas.meyer@deltadesk.org>
2021-07-22 16:00:28 +0200

    Convert tabs to spaces

--- src/test/java/org/deltadesk/TestMain.java
+++ src/test/java/org/deltadesk/TestMain.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class TestMain {
-	
-	@Test
-	public void test() {
-		Main.main(new String[0]);
-		assertTrue(true);
-	}
+
+    @Test
+    public void test() {
+        Main.main(new String[0]);
+        assertTrue(true);
+    }
 
 }
